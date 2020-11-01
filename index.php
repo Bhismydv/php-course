92c476ba860473157a5b0f5cc6db9f25dd009aa4
bhismydv <bhismydv2912@gmail.com>
2020-11-01 12:56:23 +0530

    add array

--- index.php
+++ index.php
@@ -9,6 +9,12 @@ $name="PHP Store";
 $credit=1000;
 echo "<h1>Welcome to ".$name."!</h1>";
 echo "<h2>You have ".$credit." in your wallet.</h2>";
+$products['Computer']=750;
+$products['Car']=150000;
+$products['iPhone']=1000;
+$products['Toaster']=75;
+
+echo "<p>A computer costs $".$products['Computer']."</p>"
 ?>
 </body>
 </html>
