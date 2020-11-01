50a157cb8dd24c54857f4443f867fb21452df670
bhismydv <bhismydv2912@gmail.com>
2020-11-01 12:40:38 +0530

    initial php setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP Store</title>
+</head>
+<body>
+<?php
+//using two forward slashes we create comments in php.
+//within the php tag we can insert values into your html.
+echo "<h1>welcome!</h1>";
+?>
+</body>
+</html>
